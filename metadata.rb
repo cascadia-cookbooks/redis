a0dc5c82788c86ffe0fd92d154bb954c2b0074a6
Colby Olson <me@colbyolson.com>
2016-08-31 12:34:42 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures Redis.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 source_url 'https://github.com/copious-cookbooks/redis'
 issues_url 'https://github.com/copious-cookbooks/redis/issues'
