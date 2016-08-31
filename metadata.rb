1d05bcae369513b107a51c32d2cb1798ba9319e4
Colby Olson <me@colbyolson.com>
2016-08-31 12:18:56 -0700

    remove chef_version method

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures Redis.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-chef_version '>= 12.5'
 version '0.1.0'
 
 source_url 'https://github.com/copious-cookbooks/redis'
