46d5979f54d8a171afc29e3c97d03ee4da410d8c
Colby Olson <me@colbyolson.com>
2016-11-30 11:43:27 -0800

    clean up deps recipe

--- recipes/dependencies.rb
+++ recipes/dependencies.rb
@@ -2,17 +2,15 @@ case node['platform_family']
 when 'debian'
     include_recipe 'apt::default'
 when 'rhel'
-    epel = File.exists?('/etc/yum.repos.d/epel.repo')
-
     # NOTE: EPEL (Extra Packages for Enterprise Linux)
     package 'epel-release' do
-        not_if { epel }
         action :install
-        notifies :run, 'execute[update yum]', :immediately
     end
-
-    execute 'update yum' do
-        command "yum clean all && yum distro-sync -q -y && yum update -q -y"
-        action  :nothing
+when 'fedora'
+    # NOTE: workaround for lack of Chef DNF provider on fedora >= 19
+    execute 'dnf install -y yum' do
+        command 'dnf install -y yum'
+        only_if 'test -f /usr/bin/dnf'
+        creates '/usr/bin/yum-deprecated'
     end
 end
