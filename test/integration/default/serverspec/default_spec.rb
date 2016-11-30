624867a05c329b03ecf889509fbc898cf9722a8e
Matthew Davis <mattd@copiousinc.com>
2016-11-29 21:27:07 -0800

    Use variables in tests for different platforms.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,24 +1,35 @@
 require 'spec_helper'
 
+case os[:family]
+when 'ubuntu', 'debian'
+    package_name = 'redis-server'
+    service_name = 'redis-server'
+    redis_conf = '/etc/redis/redis.conf'
+when 'redhat'
+    package_name = 'redis'
+    service_name = 'redis'
+    redis_conf = '/etc/redis.conf'
+end
+
 describe 'redis::default' do
-  describe package('redis-server') do
+  describe package(package_name) do
     it { should be_installed }
   end
 
-  describe command('redis-server --version'), :if => os[:release] == '14.04' do
+  describe command("#{package_name} --version"), :if => os[:release] == '14.04' do
     its(:stdout) { should match /2.8.4/ }
   end
 
-  describe command('redis-server --version'), :if => os[:release] == '16.04' do
+  describe command("#{package_name} --version"), :if => os[:release] == '16.04' do
     its(:stdout) { should match /3.0.6/ }
   end
 
   it 'the redis service is enabled' do
-    expect(service('redis-server')).to be_enabled
+    expect(service(service_name)).to be_enabled
   end
 
   it 'the redis service is running' do
-    expect(service('redis-server')).to be_running
+    expect(service(service_name)).to be_running
   end
 
   describe port(6379) do
@@ -33,7 +44,7 @@ describe 'redis::default' do
     expect(group('redis')).to exist
   end
 
-  describe file('/etc/redis/redis.conf') do
+  describe file(redis_conf) do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_mode '644' }
