36f3105c5cd1a0cbc675fb6359ef58381d9008f1
Colby Olson <me@colbyolson.com>
2016-12-02 15:15:00 -0800

    update test, add recipes, download redis tarball, compile!

---
+++ recipes/redis.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: cop_redis
+# Recipe:: redis
+# Author:: Copious Inc. <engineering@copiousinc.com>
+#
+
+# NOTE: systemd
+template '/etc/systemd/system/redis.service' do
+    cookbook 'cop_redis'
+    source   'systemd/redis.service.erb'
+    group    'root'
+    owner    'root'
+    mode     0644
+    backup   false
+    action   :create
+    only_if  'pidof systemd'
+end
+
+# NOTE: initd
+template '/etc/init.d/redis' do
+    cookbook 'cop_redis'
+    source   'initd/redis.init.erb'
+    group    'root'
+    owner    'root'
+    mode     0755
+    backup   false
+    action   :create
+    not_if   'pidof systemd'
+end
+
+template 'installing redis config' do
+    cookbook 'cop_redis'
+    path     node['redis']['conf_file']
+    source   'redis/redis.conf.erb'
+    group    'root'
+    owner    'root'
+    mode     0644
+    backup   false
+    action   :create
+    notifies :restart, 'service[redis]', :delayed
+end
+
+service 'redis' do
+    action [:enable, :start]
+end
